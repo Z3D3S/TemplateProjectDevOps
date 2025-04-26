afae67ec46262ea381dd4793b1026826947c4091
Z3D3S <89590642+Z3D3S@users.noreply.github.com>
2025-04-26 11:01:46 -0300

    Update BusinessService.cs

--- LacunaDevOps/Business/BusinessService.cs
+++ LacunaDevOps/Business/BusinessService.cs
@@ -1,8 +1,8 @@
-﻿using LacunaDevOps.Site.Entities;
+﻿using {{TemplateName}}.Site.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 
-namespace LacunaDevOps.Site.Business
+namespace {{TemplateName}}.Site.Business
 {
     public partial class BusinessService
     {
@@ -10,12 +10,6 @@ namespace LacunaDevOps.Site.Business
         public BusinessService(AppDbContext dbContext)
         {
             this.dbContext = dbContext;
-        }
-
-        public async Task<Recipe> GetRequireRecipe(Guid id)
-        {
-            await dbContext.Recipes.FirstOrDefaultAsync(i => i.Id == id) ?? throw new Exception($"Receita com o id: {id} não encontrado");
-
         }
     }
 }
