65def89f04f9bba8387ded96c5615dd5ca55c680
Z3D3S <89590642+Z3D3S@users.noreply.github.com>
2025-04-26 10:50:22 -0300

    Add files via upload

---
+++ LacunaDevOps/Entities/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using LacunaDevOps.Api;
+
+namespace LacunaDevOps.Site.Entities
+{
+    public class Ingredient
+    {        
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public UnitOfMeasure StandardUnitOfMeasure { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public List<IngredientInRecipe> IngredientInRecipes { get; set; } = [];
+    }
+}
---
+++ LacunaDevOps/Entities/IngredientInRecipe.cs
@@ -0,0 +1,15 @@
+﻿namespace LacunaDevOps.Site.Entities
+{
+    public class IngredientInRecipe
+    {
+        public Guid Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Guid RecipeId { get; set; }
+        public Recipe Recipe { get; set; } = null!;
+
+        public Guid IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; } = null!;
+    }
+}
