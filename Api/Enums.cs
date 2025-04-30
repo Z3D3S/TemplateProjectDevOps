2001e13b7fad4cf4146cf4cc6b00b35317a934e5
Z3D3S <89590642+Z3D3S@users.noreply.github.com>
2025-04-30 01:05:16 -0300

    Update Enums.cs

--- Api/Enums.cs
+++ Api/Enums.cs
@@ -1,20 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LacunaDevOps.Api
+namespace {{TemplateName}}.Api
 {
-   public enum UnitOfMeasure
-    {
-        Gram = 0,
-        Kilogram = 1,
-        Milliliter = 2,
-        Liter = 3,
-        Tablespoon = 4,
-        Teaspoon = 5,
-        Cup = 6,
-        Unit = 7,
-    };
 }
