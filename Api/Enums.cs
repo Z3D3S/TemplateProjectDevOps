a96a165f255641182a45b944e8c0631563e5a93b
Z3D3S <89590642+Z3D3S@users.noreply.github.com>
2025-04-30 01:00:43 -0300

    Add files via upload

---
+++ Api/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LacunaDevOps.Api
+{
+   public enum UnitOfMeasure
+    {
+        Gram = 0,
+        Kilogram = 1,
+        Milliliter = 2,
+        Liter = 3,
+        Tablespoon = 4,
+        Teaspoon = 5,
+        Cup = 6,
+        Unit = 7,
+    };
+}
