d5c095a8cba2b944df5c5c72729a13ba4a66802d
Z3D3S <89590642+Z3D3S@users.noreply.github.com>
2025-04-29 23:59:23 -0300

    Update AppDbContext.cs

--- Site/Entities/AppDbContext.cs
+++ Site/Entities/AppDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace LacunaDevOps.Site.Entities
+namespace {{TemplateName}}.Site.Entities
 {
     public class AppDbContext : DbContext
     {
@@ -12,30 +12,6 @@ namespace LacunaDevOps.Site.Entities
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder); // ← BOA PRÁTICA!
-
-            modelBuilder.Entity<Ingredient>().HasQueryFilter(i => !i.IsDeleted);
-            modelBuilder.Entity<Recipe>().HasQueryFilter(r => !r.IsDeleted);
-
-            modelBuilder.Entity<IngredientInRecipe>()
-            .HasKey(ir => new { ir.RecipeId, ir.IngredientId }); // chave composta
-
-            modelBuilder.Entity<IngredientInRecipe>()
-                .HasOne(ir => ir.Recipe)
-                .WithMany(r => r.IngredientInRecipes)
-                .HasForeignKey(ir => ir.RecipeId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<IngredientInRecipe>()
-                .HasOne(ir => ir.Ingredient)
-                .WithMany(i => i.IngredientInRecipes)
-                .HasForeignKey(ir => ir.IngredientId)
-                .OnDelete(DeleteBehavior.Cascade);
         }
-
-        public DbSet<Ingredient> Ingredients { get; set; }
-
-        public DbSet<IngredientInRecipe> IngredientInRecipes { get; set; }
-
-        public DbSet<Recipe> Recipes { get; set; }
     }
 }
